0cd72ab76f5c45cd64d19747e582b82839f475cf
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-03 20:13:15 +0200

    init class TimerExample

---
+++ src/dev/lpa/TimerExample.java
@@ -0,0 +1,13 @@
+package dev.lpa;
+
+import java.util.Timer; // had manually import
+
+public class TimerExample {
+
+  public static void main(String[] args) {
+
+    Timer timer = new Timer();
+
+
+  }
+}
