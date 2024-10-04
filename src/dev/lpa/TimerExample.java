c25423ef449107a9fc16a86d8752d7b486ba94a0
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-04 10:10:08 +0200

    documentation: ScheduledThreadPoolExecutor with one thread makes it equivalent to Timer.

--- src/dev/lpa/TimerExample.java
+++ src/dev/lpa/TimerExample.java
@@ -2,14 +2,15 @@ package dev.lpa;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class TimerExample {
 
   public static void main(String[] args) {
 
-    Timer timer = new Timer();
+//    Timer timer = new Timer(); // constructs Timer Thread
 
     TimerTask task = new TimerTask() {
       @Override
@@ -23,14 +24,20 @@ public class TimerExample {
       }
     };
 
-    timer.scheduleAtFixedRate(task, 0, 2_000); // in ms
+//    timer.scheduleAtFixedRate(task, 0, 2_000); // in ms
+
+    var executor = Executors.newSingleThreadScheduledExecutor();
+    executor.scheduleAtFixedRate(task, 0, 2, TimeUnit.SECONDS);
+    // TimerTask implements Runnable interface, but can not use Runnable in Timer instance method
 
     try {
       Thread.sleep(12_000);
     } catch (InterruptedException e) {
       e.printStackTrace();
-    }
+    } finally {
 
-    timer.cancel();
+//    timer.cancel();
+      executor.shutdown();
+    }
   }
 }
