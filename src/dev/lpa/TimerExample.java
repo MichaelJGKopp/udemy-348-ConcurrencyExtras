64babc5a8e32c948fa0ba1914189005148cc5714
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-04 09:55:33 +0200

    add a Timer Example

--- src/dev/lpa/TimerExample.java
+++ src/dev/lpa/TimerExample.java
@@ -1,6 +1,9 @@
 package dev.lpa;
 
-import java.util.Timer; // had manually import
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TimerExample {
 
@@ -8,6 +11,26 @@ public class TimerExample {
 
     Timer timer = new Timer();
 
+    TimerTask task = new TimerTask() {
+      @Override
+      public void run() {
+        String threadName = Thread.currentThread().getName();
+        DateTimeFormatter formatter =
+          DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+        System.out.println(threadName + " Timer task executed at: " +
+          formatter.format(LocalDateTime.now()));
+      }
+    };
+
+    timer.scheduleAtFixedRate(task, 0, 2_000); // in ms
+
+    try {
+      Thread.sleep(12_000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    timer.cancel();
   }
 }
